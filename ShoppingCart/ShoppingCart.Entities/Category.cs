a3497ed4aeb4786690cdd2e26d12a95cc7b93daa
ritika121501 <ritika120115@gmail.com>
2025-01-22 06:21:53 +0530

    Added new Project

---
+++ ShoppingCart/ShoppingCart.Entities/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace ShoppingCart.Entities
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [MaxLength(30)]
+        [Required]
+        [DisplayName("Category name")]
+        public string Name { get; set; }
+    }
+}
---
+++ ShoppingCart/ShoppingCart.Entities/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace ShoppingCart.Entities
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ISBN { get; set; }
+        public string Author { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        [ValidateNever]
+        public Category Category { get; set; }
+
+        [ValidateNever]
+        public List<ProductImage> ProductImages { get; set; }
+    }
+}
