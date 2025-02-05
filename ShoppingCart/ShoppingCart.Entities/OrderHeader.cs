5ebf59589a253f567d0efea5bce8e7ca5605ac70
ritika121501 <ritika120115@gmail.com>
2025-02-05 06:02:19 +0530

    changes

---
+++ ShoppingCart/ShoppingCart.Entities/OrderHeader.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Entities
+{
+    public class OrderHeader
+    {
+        [Key]
+        public int OrderHeaderId { get;set; }
+        public string ApplicationUserId { get; set; }
+        [ForeignKey("ApplicationUserId")]
+        [ValidateNever]
+        public ApplicationUser ApplicationUser { get; set; }
+
+        public DateTime OrderDate { get; set; }
+        public DateTime ShippingDate { get; set; }
+        public decimal OrderTotal {  get; set; }
+        public string? OrderStatus {  get; set; }
+        public string? PaymentStatus {  get; set; }
+        public string? Carrier { get; set; }
+        public DateTime PaymentDate { get;set; }
+        public string? PhoneNumber { get; set; }
+        public string? StreetAddress {  get; set; }
+        public string? City { get; set; }
+        public string? Name { get; set; }
+    }
+}
--- ShoppingCart/ShoppingCart/Controllers/CartController.cs
+++ ShoppingCart/ShoppingCart/Controllers/CartController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ShoppingCart.Entities;
+using ShoppingCart.Entities.ViewModel;
 using ShoppingCart.Repository;
 using System.Security.Claims;
 
@@ -19,19 +20,26 @@ namespace ShoppingCart.Controllers
         {
             var claimsIdentity = (ClaimsIdentity)User.Identity;
             var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
-            List<ShoppingKart> sk = _unitOfWork.Shoppingkart.GetAllExpression(u => u.ApplicationUserId == userId,
-                includeProperties: "Product").ToList();
+
+            ShoppingCartVM shoppingCartVM = new ShoppingCartVM()
+            {
+                ShoppingKartsList = _unitOfWork.Shoppingkart.GetAllExpression(u => u.ApplicationUserId == userId,
+                includeProperties: "Product").ToList(),
+                OrderHeader = new()
+
+            };
 
             IEnumerable<ProductImage> productImages =
                 _unitOfWork.ProductImage.GetAllExpression();
 
-            foreach(var cart in sk)
+            foreach(var cart in shoppingCartVM.ShoppingKartsList)
             {
                 cart.Product.ProductImages = productImages.Where(x => x.ProductId == cart.Product.ProductId).ToList();
                 cart.Price = GetPriceBasedOnQuantity(cart);
-
+                shoppingCartVM.OrderHeader.OrderTotal += (cart.Price * cart.Count);
+                //shoppingCartVM.OrderHeader.OrderTotal = shoppingCartVM.OrderHeader.OrderTotal + (cart.Price * cart.Count);
             }
-            return View(sk);
+            return View(shoppingCartVM);
         }
 
         public IActionResult Plus(int cartId)
@@ -63,7 +71,7 @@ namespace ShoppingCart.Controllers
         private decimal GetPriceBasedOnQuantity(ShoppingKart shoppingKart)
         {
             decimal finalPrice = 0;
-            if(shoppingKart == null)
+            if(shoppingKart != null)
             {
                 finalPrice= shoppingKart.Count * shoppingKart.Price;
             }
