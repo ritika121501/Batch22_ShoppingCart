7bdc143830167a3eff84f62c1f249e4c91a29625
ritika121501 <ritika120115@gmail.com>
2025-03-13 05:18:32 +0530

    Added the code to keep acount

---
+++ ShoppingCart/ShoppingCart/ViewComponents/ShoppingCartViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShoppingCart.Repository;
+using System.Security.Claims;
+namespace ShoppingCart.ViewComponents
+{
+    public class ShoppingCartViewComponent : ViewComponent
+    {
+
+        private readonly IUnitOfWork _unitOfWork;
+        public ShoppingCartViewComponent(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            if (claim != null)
+            {
+                var count = _unitOfWork.Shoppingkart.GetAllExpression(u => u.ApplicationUserId == claim.Value).Count();
+                return View(count);
+            }
+            return View(0);
+        }
+    }
+}
